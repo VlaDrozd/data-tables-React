7c3971507644d2c4138d767a62a50da644091b43
VlaDrozd <vladrozd2000@gmail.com>
2020-10-06 00:34:51 +0300

    feat: add state provider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
-import { PeoplePage } from "./pages/PeoplePage/PeoplePage";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from './store/reducer';
+import thunk from "redux-thunk";
 
 function App() {
-
-  
+  const store = createStore(rootReducer, applyMiddleware(thunk));
   return (
+    <Provider store={store}>
       <div className="App">
-        <Router>
-          <Navbar></Navbar>
-          <PeoplePage />
-        </Router>
-      </div>
+      <Router>
+        <Navbar></Navbar>
+      </Router>
+    </div>
+    </Provider>
   );
 }
 
